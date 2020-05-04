df0492afe9e271fae7d318ae07f489a3bae3a3f1
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-05 00:10:44 +0800

    新增 user name 欄位 調整部分repository view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_122427) do
+ActiveRecord::Schema.define(version: 2020_05_04_155733) do
 
   create_table "repositories", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_122427) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
