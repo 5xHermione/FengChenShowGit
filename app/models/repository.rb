32680fc71f1bd784af4f4f3602380ad79413810c
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-06 19:06:30 +0800

    user 底下的 repo 不能有相同 title

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,4 +2,13 @@ class Repository < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :issues
+  validate :check_unique_title
+
+  def check_unique_title
+    # blank? 檢查東西是不是沒有或是空值或是 nil
+    # present? 檢查東西是不是存在
+    if user.repositories.find_by(title: title).present?
+      errors.add(:title, "title can't be exists!")
+    end
+  end
 end
