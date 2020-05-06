553bb5b5175c020a3fac29ec88ffb8a2fca66ba7
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-06 23:51:47 +0800

    issue routes 新增

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,14 @@ Rails.application.routes.draw do
   get "/:user_name/:repository_title/edit", to: "repositories#edit", as: "edit_repo" # repo的edit
   delete "/:user_name/:repository_title/destroy", to: "repositories#destroy", as: "destroy_repo" # repo的destroy
   patch "/:user_name/:repository_title/update", to: "repositories#update", as: "update_repo"  # repo的update
+
   get "/:user_name/:repository_title/issues", to: "issues#index", as: "issues" # issue的index
+  get "/:user_name/:repository_title/issues/new", to: "issues#new", as: "new_issues" #issue的new
+  post "/:user_name/:repository_title/issues/create", to: "issues#create", as: "create_issues" #issue的create
+  get ":user_name/:repository_title/issues/:id", to: "issues#show", as: "show_issues" #issue的show
+  get ":user_name/:repository_title/issues/:id/edit", to: "issues#edit", as: "edit_issues" #issue的edit
+  delete "/:user_name/:repository_title/issues/:id/destroy", to: "issues#destroy", as: "destroy_issues" #issue的destroy
+  patch "/:user_name/:repository_title/issues/:id/update", to: "issues#update", as: "update_issues" #issue的update
 
 end
 
