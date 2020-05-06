7abd7f163df7149fe3d6dac782297d94b588a38e
eric <yellowtry123@gmail.com>
2020-05-06 16:11:05 +0800

    jq-import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,15 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 require("style");
-require("jquery");
-require("bootstrap");
 require("popper.js");
+
+
+//bootstrap 
+
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+//JQ
+import "jquery"
+import $ from 'jquery'
+window.$ = $
