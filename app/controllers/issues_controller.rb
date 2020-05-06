38abfba9de1ef4674a6fe37ca09692b04a647d27
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-06 11:48:40 +0800

    routes 調整

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,8 @@
 class IssuesController < ApplicationController 
   def index
-    @issues = current_user.repositories.find_by(params[:repository_title]).issues.first
+    @issues = current_user.repositories.find_by(params[:repository_title]).issues
+    p "----------------"
+    p @issues
+    p "----------------------"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   resources :repositories
   devise_for :users
+
   root "home#index"
 
   get "/:user_name", to: "repositories#index"
-  get "/:user_name/:repository_title/issues", to: "issues#index", as: "issues"
+  get "/:user_name/:repository_title", to: "repository#show"
+  get "/:user_name/:repository_title/issues", to: "issues#index"
 end
 
 
-# /:user_name/:repo_name/:issue_id   GET Issue/index
+# /:user_name/:repo_name/issues  GET Issue/index
 # params[:user_name]/params[:repo_title]/params[:issue_id]
