ca2bf4cb83978d23cefd9e8620939ca5b12d700d
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-07 10:36:37 +0800

    route new 與 index 路徑重複問題處理

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
 
-  get "/:user_name", to: "repositories#index", as: "user" # repo的index
-  get "/:user_name/new", to: "repositories#new", as: "new_repo"   # repo的new
-  post ":user_name/create", to: "repositories#create", as: "create_repo" # repo的create
-  get "/:user_name/:repository_title", to: "repositories#show", as: "repo"           # repo的show
-  get "/:user_name/:repository_title/edit", to: "repositories#edit", as: "edit_repo" # repo的edit
-  delete "/:user_name/:repository_title/destroy", to: "repositories#destroy", as: "destroy_repo" # repo的destroy
-  patch "/:user_name/:repository_title/update", to: "repositories#update", as: "update_repo"  # repo的update
+  get "/repo/:user_name", to: "repositories#index", as: "user" # repo的index
+  get "/repo/:user_name/new", to: "repositories#new", as: "new_repo"   # repo的new
+  post "/repo/:user_name/create", to: "repositories#create", as: "create_repo" # repo的create
+  get "/repo/:user_name/:repository_title", to: "repositories#show", as: "repo"           # repo的show
+  get "/repo/:user_name/:repository_title/edit", to: "repositories#edit", as: "edit_repo" # repo的edit
+  delete "/repo/:user_name/:repository_title/destroy", to: "repositories#destroy", as: "destroy_repo" # repo的destroy
+  patch "/repo/:user_name/:repository_title/update", to: "repositories#update", as: "update_repo"  # repo的update
 
   get "/:user_name/:repository_title/issues", to: "issues#index", as: "issues" # issue的index
   get "/:user_name/:repository_title/issues/new", to: "issues#new", as: "new_issues" #issue的new
