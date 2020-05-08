e16635649bcb92c29781d067f1b4084e335f6cba
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-08 13:31:04 +0800

    將 devise 方法重現到 home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
-class HomeController < ApplicationController 
-  def index
+class HomeController < ApplicationController
+  helper_method :resource_name, :resource, :devise_mapping
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
