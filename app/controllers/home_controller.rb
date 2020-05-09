5257af25d4831ac4f35704fa803803f7e681c671
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-10 07:58:37 +0800

    home controller 小修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   helper_method :resource_name, :resource, :devise_mapping
 
   def index
-    @repositories = current_user.repositories
+    @repositories = current_user.repositories if user_signed_in?
   end
 
   def resource_name
