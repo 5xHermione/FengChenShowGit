0de905a950fcdfc3d9d35fa3ecdcda47d3cd514b
eric <yellowtry123@gmail.com>
2020-05-11 12:10:05 +0800

    issue new

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,4 +2,33 @@ class IssuesController < ApplicationController
   def index
     @issues = current_repository.issues
   end
+
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+    # @issue = current_user.issues.build(issue_params)
+    @issue = Repository.find_by(id: params[:repository_id]).issues.build(issue_params)
+    # @issue.update(user_id: current_user.id)
+    
+
+    if @issue.save
+      redirect_to repository_issues_path, notice: '已建立新專案！' 
+    else
+      render :new
+    end
+
+  end
+
+
+  private
+  
+
+  def issue_params
+    clean_params = params.require(:issue).permit(:name, :description)
+  end
+
+
+
 end
