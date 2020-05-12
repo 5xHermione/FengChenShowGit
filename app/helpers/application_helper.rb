69b0a413732e5df62cc46c416883b24d0fddc5d9
Shelly <kai2004@hotmail.com>
2020-05-12 17:12:49 +0800

    index first commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+	# def background_class
+	# 	if params[:controller]=="home" && params[:action] == "index" && !(user_signed_in?)
+	# 		return "back"
+	# 	else
+	# 		return ""
+	# 	end
+	# end
+
+  #  def backcolor_class
+	# 	if params[:controller]=="home" && params[:action] == "index" && !(user_signed_in?)
+	# 		return "backcolor"
+	# 	else
+	# 		return ""
+	# 	end
+	# end
+
+	def backcolor_class
+		return "" if user_signed_in?
+		return "backcolor" if homepage?
+		""
+	end
+
+
+	def background_class
+		return "" if user_signed_in?
+		return "back" if homepage?
+		""
+	end
+
+
+
+	def homepage?
+		params[:controller]=="home" && params[:action] == "index"
+	end
+
 end
