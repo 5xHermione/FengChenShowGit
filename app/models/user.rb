3583680df6f7438273b86eff96b24a2dfcc7a624
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-12 17:38:30 +0800

    新增 migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,  :recoverable, :rememberable, :validatable, :registerable, :confirmable
   has_many :repositories
   validates :name, presence: true, uniqueness: true
 end
