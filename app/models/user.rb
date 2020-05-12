6f2f37926f02853fe39b768c94c86e417e3e421b
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-12 12:45:29 +0800

    正規表達式調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :repositories
   validates :email, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "format does not match."}
   validates :name, presence: true, uniqueness: true,
-                                   format: { with: /A[a-zA-Z0-9_]/, message: "only allows alphabets, numbers and underscore." }
+                                   format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
 end
