f2118ed13c79bf732f7def2bc2196fb86a40d532
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-12 12:15:19 +0800

    用正規表達式 控制可以寫進資料庫的資料寫法

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -3,6 +3,7 @@ class Repository < ApplicationRecord
   belongs_to :user
   has_many :issues
   validate :check_unique_title
+  validates :title, format: { with: /A[a-zA-Z0-9_]/, message: "only allows alphabets, numbers and underscore." }
 
   def check_unique_title
     # blank? 檢查東西是不是沒有或是空值
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :repositories
-  validates :name, presence: true, uniqueness: true
+  validates :email, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "format does not match."}
+  validates :name, presence: true, uniqueness: true,
+                                   format: { with: /A[a-zA-Z0-9_]/, message: "only allows alphabets, numbers and underscore." }
 end
