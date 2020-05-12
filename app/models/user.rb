6dbf39d4f52d52572e135433f70d405066ada253
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-12 17:42:11 +0800

    user email 新增 validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :repositories
   validates :email, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "format does not match."}
+  validates :email, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true,
                                    format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
 end
