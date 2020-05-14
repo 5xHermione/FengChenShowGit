d3dc5960760c5ba8f3eb39100ce795a074138fb0
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-14 12:04:12 +0800

    user edit 連結 ，edit 功能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # 相關資訊：https://www.rubydoc.info/github/plataformatec/devise/Devise/ParameterSanitizer
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   private
