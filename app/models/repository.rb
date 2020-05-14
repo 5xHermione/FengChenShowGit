133bf6529296363b5aa779adc0767580d90fe1e4
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-14 18:18:28 +0800

    改寫 routes 作業中

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,6 +4,12 @@ class Repository < ApplicationRecord
   has_many :issues
   validate :check_unique_title
   validates :title, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
+  
+  before_save :save_slug_with_title
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
 
   def check_unique_title
     # blank? 檢查東西是不是沒有或是空值
@@ -11,5 +17,9 @@ class Repository < ApplicationRecord
     if user.repositories.find_by(title: title).present?
       errors.add(:title, ": This title already exitsts!")
     end
+  end
+
+  def save_slug_with_title
+    self.slug = self.title if save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sessions: "sessions", sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
+  devise_for :users, path: '', 
+                     path_names: { 
+                        sessions: "sessions", 
+                        sign_up: 'register', 
+                        sign_in: 'login', 
+                        sign_out: 'logout'
+                      }, 
+                      controllers: {
+                        registrations: "user/registrations" 
+                      }
   root "home#index"
 
-  resources :repositories do
-    resources :issues
+  # resources :repositories do
+  #   resources :issues
+  # end
+
+  scope '/:user_id' do
+    resources :repositories do
+      resources :issues
+    end
   end
+
 end
+# user save 的時候要存 slug 欄位。
+# routes 要傳 id 東西進去，他會去找 slug 欄位。
