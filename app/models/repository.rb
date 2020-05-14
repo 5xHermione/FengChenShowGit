76c734e2580d96cee06d6c462f9697d8dac191a7
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-14 14:07:59 +0800

    寫法更正

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,8 +1,8 @@
 class Repository < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :user,
+                                                  message: ": This title already exitsts!" }
   belongs_to :user
   has_many :issues
-  validate :check_unique_title
   validates :title, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
 
   extend FriendlyId
@@ -21,3 +21,4 @@ class Repository < ApplicationRecord
     end
   end
 end
+
