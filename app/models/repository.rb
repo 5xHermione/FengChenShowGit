0a58bf177df48556dba1806df8f913d05ee5252e
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-15 01:02:23 +0800

    在創建時給 repo slug 欄位賦值

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,7 +2,7 @@ class Repository < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :issues
-  # validate :check_unique_title
+  validate :check_unique_title
   validates :title, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
 
   extend FriendlyId
@@ -10,14 +10,14 @@ class Repository < ApplicationRecord
   validates_exclusion_of :title, in: Blacklist.pluck(:name), message: ": This title is not allowed."
 
   def should_generate_new_friendly_id?
-    title_changed?
+    slug.blank? || title_changed?
   end
 
-  # def check_unique_title
-  #   # blank? 檢查東西是不是沒有或是空值
-  #   # present? 檢查東西是不是存在
-  #   if user.repositories.find_by(title: title).present?
-  #     errors.add(:title, ": This title already exitsts!")
-  #   end
-  # end
+  def check_unique_title
+    # blank? 檢查東西是不是沒有或是空值
+    # present? 檢查東西是不是存在
+    if user.repositories.find_by(title: title).present?
+      errors.add(:title, ": This title already exitsts!")
+    end
+  end
 end
