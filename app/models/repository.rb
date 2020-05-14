6223b4761c00d82d5cb6388f98a9ff5be23ed8dd
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-14 14:07:59 +0800

    寫法更正

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,15 +1,7 @@
 class Repository < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :user,
+                                                  message: ": This title already exitsts!" }
   belongs_to :user
   has_many :issues
-  validate :check_unique_title
   validates :title, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
-
-  def check_unique_title
-    # blank? 檢查東西是不是沒有或是空值
-    # present? 檢查東西是不是存在
-    if user.repositories.find_by(title: title).present?
-      errors.add(:title, ": This title already exitsts!")
-    end
-  end
 end
