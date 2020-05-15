99aaaa4d36d36235d7d19ca5a54904877b36356e
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-15 12:40:59 +0800

    建立 seed 檔，快速建立 blacklist 資料

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+["edit", "login", "logout", "password", "new", "cancel", "register", "confirmation", "repositories", "issues", "rails"].each do |word|
+  Blacklist.create(name: word)
+end
+
+# clone 專案後執行 rails db:seeds 用於建立黑名單資料
