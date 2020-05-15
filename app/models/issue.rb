a6dc92a54abe683ee525ca5eb1e0c63d6302778c
eric <yellowtry123@gmail.com>
2020-05-15 12:26:57 +0800

    issue開關初階段完成&issue路經修改調整

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,7 @@ class Issue < ApplicationRecord
   validates :name, presence: true
   belongs_to :repository
 
+<<<<<<< HEAD
   def toggle_status
 
     if self.status == "Close"
@@ -10,8 +11,14 @@ class Issue < ApplicationRecord
       self.status = "Close"  
     end
 
+=======
+  enum status: {close: 'Close' , publish: 'Reopen',}
+
+  def toggle_status
+    self.status = self.publish? ? :close : :publish
+>>>>>>> issue開關初階段完成&issue路經修改調整
   end
-  
+
   # soft_delete
   # scope :not_deleted, -> { where(soft_deleted: false) }
   # scope :deleted, -> { where(soft_deleted: true) }
