438c1069210cfa83cb2328efeef86cfede1da3ae
Lawa0921 <bag571ivy3470@gmail.com>
2020-05-15 14:55:50 +0800

    讓 repo 刪除時可以刪除所有關聯的 issue

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,7 +2,7 @@ class Repository < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :user,
                                                   message: ": This title already exitsts!" }
   belongs_to :user
-  has_many :issues
+  has_many :issues, dependent: :delete_all
   validates :title, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
 
   extend FriendlyId
