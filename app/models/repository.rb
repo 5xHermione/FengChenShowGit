3667845300ccc247d5c46f563fa7e4b50b665a96
Lawa0921 <bag571ivy3470@gmail,com>
2020-05-19 19:58:52 +0800

    位置調整

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,10 +2,10 @@ class Repository < ApplicationRecord
   extend FriendlyId
   validates :title, presence: true, uniqueness: { scope: :user,
                                                   message: ": This title already exitsts!" }
-  belongs_to :user
-  has_many :issues, dependent: :destroy
   validates :title, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." },
                     exclusion: { in: Blacklist.pluck(:name), message: ": Please change another repository title."}
+  belongs_to :user
+  has_many :issues, dependent: :destroy
 
   friendly_id :title, use: :slugged
   before_save :convert_slug_to_same_title
@@ -15,7 +15,6 @@ class Repository < ApplicationRecord
   end
 
   private
-
   def convert_slug_to_same_title
     self.slug = title if slug != title
   end
