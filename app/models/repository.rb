e90bc5270ee4979b3e2199cfcf68889743da77fe
Lawa0921 <bag571ivy3470@gmail,com>
2020-05-19 19:53:05 +0800

    validates 寫法整合

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,13 +1,13 @@
 class Repository < ApplicationRecord
+  extend FriendlyId
   validates :title, presence: true, uniqueness: { scope: :user,
                                                   message: ": This title already exitsts!" }
   belongs_to :user
   has_many :issues, dependent: :destroy
-  validates :title, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
+  validates :title, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." },
+                    exclusion: { in: Blacklist.pluck(:name), message: ": Please change another repository title."}
 
-  extend FriendlyId
   friendly_id :title, use: :slugged
-  validates_exclusion_of :title, in: Blacklist.select(:name), message: ": Please change another repository title."
   before_save :convert_slug_to_same_title
 
   def should_generate_new_friendly_id?
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   has_many :repositories, dependent: :destroy
   validates :email, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "format does not match."}
   validates :email, presence: true, uniqueness: true
-  validates :name, presence: true, uniqueness: true,
-                                   format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." }
-  validates_exclusion_of :name, in: Blacklist.select(:name), message: ": Please change another user name."
+  validates :name, presence: true, 
+                   uniqueness: true,
+                   format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows alphabets, numbers and underscore." },
+                   exclusion: { in: Blacklist.pluck(:name), message: ": Please change another user name."}
+  # validates_exclusion_of :name, in: Blacklist.select(:name), message: ": Please change another user name."
 end
