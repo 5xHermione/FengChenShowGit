e44b5590d200aaa53383ccb9e22d03067f6eb95b
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-05-22 17:05:08 +0800

    feat: deploy weback with capistrano 在 deploy.rb 與 application.html.erb 增加設定，讓 webpack 可以透過 capistrano 成功在部署前打包檔案

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,3 +39,16 @@ set :default_env, { path: "/usr/local/ruby-2.6.5/bin:$PATH" }
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
