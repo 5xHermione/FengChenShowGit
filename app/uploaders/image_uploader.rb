b87bca5b739e20f7c32028795493c91ff614827d
Shelly <kai2004@hotmail.com>
2020-05-23 20:50:37 +0800

    圖片自動裁切

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -22,16 +22,21 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  # process scale: [80, 80]
   #
   # def scale(width, height)
   #   # do something
   # end
+  process resize_to_fit: [200, 200]
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+
+  version :thumb_middle do
+    process resize_to_fit: [100, 100]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
