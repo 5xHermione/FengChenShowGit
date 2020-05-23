eafb4b7bfd19602a93372a1e1de296327df21c95
Shelly <kai2004@hotmail.com>
2020-05-23 16:17:34 +0800

    增加image欄位

---
+++ db/migrate/20200523080557_add_avatar_users.rb
@@ -0,0 +1,5 @@
+class AddAvatarUsers < ActiveRecord::Migration[6.0]
+  def change
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_080030) do
+ActiveRecord::Schema.define(version: 2020_05_23_081147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2020_05_20_080030) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "avatar"
+    t.string "image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
