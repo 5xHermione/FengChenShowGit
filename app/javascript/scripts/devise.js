d861b56df3078f059505b8c97ac0ffe9efde69bd
eric <yellowtry123@gmail.com>
2020-05-25 20:25:26 +0800

    驗證提示僅在註冊

--- app/javascript/scripts/devise.js
+++ app/javascript/scripts/devise.js
@@ -4,7 +4,7 @@ $().ready(function(){
   $(document).on("click change", function(){
     
     let rule1 = /^([\w\.\-]){1,64}\@([\w\.\-]){1,64}$/;
-    $("#user_email").blur(function(){
+    $(".email-signup").blur(function(){
       if(rule1.test($(this).val())){
         $(this).css("border-color","")
         $('.notice1').text('')
@@ -15,7 +15,7 @@ $().ready(function(){
     })
   
     let rule2 = /^.{6,12}$/;
-    $("#user_password").blur(function(){
+    $(".password-signup").blur(function(){
       if(rule2.test($(this).val())){
         $(this).css("border-color","")
         $('.notice2').text('')
@@ -25,8 +25,8 @@ $().ready(function(){
       }
     })
 
-    $("#user_password_confirmation").blur(function(){    
-      if($("#user_password").val()!=$("#user_password_confirmation").val()){
+    $(".password-confirmation-signup").blur(function(){    
+      if($(".password-signup").val()!=$(".password-confirmation-signup").val()){
         $('.notice3').text('*與密碼不相同')
         $(this).css("border-color","red")
       }else{ 
@@ -35,30 +35,20 @@ $().ready(function(){
       }
     })
 
-    let rule3 = /^.{3,20}$/;
-    $("#user_name").blur(function(){
+    let rule3 = /^\w+$/;
+    $(".username-signup").blur(function(){
       if(rule3.test($(this).val())){
         $(this).css("border-color","")
         $('.notice4').text('')      
       }else{
-          $('.notice4').text('*請輸入3個以上任意文字')
+          $('.notice4').text('請輸入英文大小寫"_"')
           $(this).css("border-color","red")
       }
     })
-
-    if(($("#user_name").val() != "")&&($("#user_name").val()!= null)){
-      
-      return true;
-  }else if(($("#user_name").val() == "" )||($("#user_name").val()== null)){
-      $('.notice4').text('')  
-      return false;
-  }
-
-
   })
 })
 
- 
+
 
 
 
