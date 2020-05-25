e768e3bf50f3ea453bd5ecd78b1afb3a195609d4
Lawa0921 <bag571ivy3470@gmail,com>
2020-05-25 17:23:42 +0800

    comment create 調整完畢

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,13 @@ class CommentsController < ApplicationController
   def create
     if params[:pull_request_id]
       @comment = PullRequest.find(params[:pull_request_id].to_i).comments.build(comment_params)
+      comment_save(@comment)
+      redirect_to repository_pull_request_path(id: params[:pull_request_id])
     else
       @comment = Issue.find(params[:issue_id].to_i).comments.build(comment_params)
+      comment_save(@comment)
+      redirect_to repository_issue_path(id: params[:issue_id])
     end
-
-    @comment.user_id = current_user.id
-    
-    if @comment.save
-      flash[:notice] = 'You have created a comment.'
-    else
-      flash[:notice] = 'something wrong.'
-    end
-    redirect_to repository_pull_request_path(id: params[:pull_request_id])
   end
 
   def update
@@ -25,5 +20,14 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def comment_save(comment)
+    comment.user_id = current_user.id
+    if comment.save
+      flash[:notice] = 'You have created a comment.'
+    else
+      flash[:notice] = 'something wrong.'
+    end
   end
 end
