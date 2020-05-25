862fabb6cfe1244b2f88f8989c2c631227399fb0
Lawa0921 <bag571ivy3470@gmail,com>
2020-05-25 17:01:14 +0800

    文字修正、圖示更換

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,9 +7,9 @@ class LikesController < ApplicationController
     liked_repository = find_user.repositories.find_by(title: params[:repository_id])
     like_relationship = Like.new(repository_id: liked_repository.id, user_id: current_user.id)
     if like_relationship.save
-      flash[:notice] = "Like success"
+      flash[:notice] = "Like success!"
     else
-      flash[:notice] = "Like unsuccess"
+      flash[:notice] = " Fail to like this repository."
     end
     redirect_to repository_path(id: current_repository)
   end
@@ -17,9 +17,9 @@ class LikesController < ApplicationController
   def destroy
     like_relationship = Like.find_by(repository_id: current_repository.id, user_id: current_user.id)
     if like_relationship.destroy
-      flash[:notice] = "Unlike success"
+      flash[:notice] = "Unlike success!"
     else
-      flash[:notice] = "Unlike unsuccess"
+      flash[:notice] = "Fail to unlike this repository."
     end
     redirect_to repository_path(id: current_repository)
   end
