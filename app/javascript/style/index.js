969f48abd9085efb9232889d89c496e99e258849
Shelly <kai2004@hotmail.com>
2020-05-26 12:25:06 +0800

    改calss名稱this-is-navbar

--- app/javascript/style/index.js
+++ app/javascript/style/index.js
@@ -4,7 +4,7 @@ import "./navindexfooter.scss";
 import "./repository_show.scss";
 import "./logged_in.scss"
 import "./repo-nav.scss";
-import "./enjoy.scss";
+import "./public.scss";
 import "./issue_show.scss";
 import "./logged_in_index.scss";
 
