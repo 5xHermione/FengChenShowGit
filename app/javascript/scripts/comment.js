6f530a9d4541bf04825cbb5e832bcc9cf93b3ede
Lawa0921 <bag571ivy3470@gmail,com>
2020-05-26 17:12:47 +0800

    調整 turbolinks 問題

--- app/javascript/scripts/comment.js
+++ app/javascript/scripts/comment.js
@@ -1,14 +1,14 @@
-$().ready(function(){
-  let editButton = $(".comment-edit-button")
-  let cancelButton = $(".comment-cancel-button")
+$(document).on("turbolinks:load", function () {
+  let editButton = $(".comment-edit-button");
+  let cancelButton = $(".comment-cancel-button");
 
-  editButton.click(function(){
-    $(".comment-info-container").hide()
-    $(".comment-edit-container").show()
-  })
+  editButton.click(function () {
+    $(".comment-info-container").hide();
+    $(".comment-edit-container").show();
+  });
 
-  cancelButton.click(function() {
-    $(".comment-edit-container").hide()
-    $(".comment-info-container").show()
-  })
-})
+  cancelButton.click(function () {
+    $(".comment-edit-container").hide();
+    $(".comment-info-container").show();
+  });
+});
