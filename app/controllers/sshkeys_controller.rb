44d413779ac6d35cf154b678dae7ce8a4e2a691b
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-05-27 07:42:55 +0800

    feat: sshkey new and create action 讓使用者可以新增 SSH key

--- app/controllers/sshkeys_controller.rb
+++ app/controllers/sshkeys_controller.rb
@@ -1,13 +1,23 @@
 class SshkeysController < ApplicationController
-
   def new
     @sshkey = Sshkey.new
   end
 
   def create
+    @sshkey = User.find(current_user.id).sshkeys.build(ssh_key_params)
+    if @sshkey.save
+      redirect_to edit_user_registration_path, notice: "New SSH key is added!"
+    else
+      render :new
+    end
   end
 
   def destroy
     
+  end
+
+  private
+  def ssh_key_params
+    params.require(:sshkey).permit(:name, :key)
   end
 end
