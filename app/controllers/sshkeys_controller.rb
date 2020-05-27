050ab25b495efde9f937acba5d9671018bf82ffc
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-05-27 11:30:33 +0800

    feat: write user ssh key to server authorized key 用環境變數設定 authorized_keys 檔案位置，當使用者新增或刪除 ssh key 的時候，將資料庫所有 ssh key 撈出來重新寫進 authorized_keys

--- app/controllers/sshkeys_controller.rb
+++ app/controllers/sshkeys_controller.rb
@@ -6,7 +6,7 @@ class SshkeysController < ApplicationController
   def create
     @sshkey = User.find(current_user.id).sshkeys.build(ssh_key_params)
     if @sshkey.save
-      #save ssh key to authorized key
+      update_authorized_keys
       redirect_to edit_user_registration_path, notice: "New SSH key is added!"
     else
       render :new
@@ -15,11 +15,21 @@ class SshkeysController < ApplicationController
 
   def destroy
     Sshkey.find(params[:id]).destroy
+    update_authorized_keys
     redirect_to edit_user_registration_path, notice: "The SSH key is deleted!"
   end
 
   private
   def ssh_key_params
     params.require(:sshkey).permit(:name, :key)
+  end
+
+  def update_authorized_keys
+    #save ssh key to authorized keys
+    authorized_keys_file = File.open(ENV["AUTHORIZED_KEYS_PATH"])
+    File.write(authorized_keys_file, "", mode: "w")
+    Sshkey.all.each do |ssh|
+      File.write(authorized_keys_file, "#{ssh.key}\n", mode: "a")
+    end
   end
 end
