896a69f0fab3805a33d45b758340df9f8006e6fa
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-05-28 15:08:39 +0800

    feat: 將刪除使用者的功能刪掉 1. 刪除使用者若使用客製化的 devise controller 會有問題，考量開發主功能時間不夠，決定先不讓使用者刪除帳號 2. 若將全部文字刪掉會跑版，之後會再處理

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-  # PUT /resource
   def update
     old_user_name = current_user.name
     super
     `mv #{ENV["GIT_SERVER_PATH"]}/#{old_user_name} #{ENV["GIT_SERVER_PATH"]}/#{params[:user][:name]}`
   end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
 
 end
