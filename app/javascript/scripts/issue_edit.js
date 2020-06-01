a568e9b35c7256ed9429a7871dbab1860be9e729
eric <yellowtry123@gmail.com>
2020-06-02 00:37:13 +0800

    issue_textarea_ajax

--- app/javascript/scripts/issue_edit.js
+++ app/javascript/scripts/issue_edit.js
@@ -13,4 +13,26 @@ $().ready(function(){
     $("#issue_update").hide();
     $(".issue_name_active").hide();
   });
+
+  $("#issue_description_edit").click(function(){
+    $("#issue_description_update").show();
+    $(".issue_description_active").show();
+    $("#issue_description_edit").hide();
+    $(".issue_description_inactive").hide();
+    $(".issue_description_active textarea[type=text_area]").focus();
+  });
+
+  $("#issue_description_update").click(function(){
+    $("#issue_description_edit").show();
+    $(".issue_description_inactive").show();
+    $("#issue_description_update").hide();
+    $(".issue_description_active").hide();
+  });
+
+
+
+
+
+
+
 });
