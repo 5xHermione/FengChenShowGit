c158bd110947193b87c38f8c12c57f6d877d7639
eric <yellowtry123@gmail.com>
2020-06-04 18:32:15 +0800

    issue_show_ui_v1

--- app/javascript/scripts/issue_edit.js
+++ app/javascript/scripts/issue_edit.js
@@ -17,6 +17,7 @@ $(document).on("turbolinks:load", function(){
     $("#issue_description_edit").click(function(){
       $("#issue_description_update").show();
       $(".issue_description_active").show();
+      $(".developer-comment-cancel-btn").show();
       $("#issue_description_edit").hide();
       $(".issue_description_inactive").hide();
       $(".issue_description_active textarea[type=text_area]").focus();
@@ -27,6 +28,7 @@ $(document).on("turbolinks:load", function(){
       $(".issue_description_inactive").show();
       $("#issue_description_update").hide();
       $(".issue_description_active").hide();
+      $(".developer-comment-cancel-btn").hide();
     });
 
 });
