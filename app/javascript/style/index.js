5233a7b9db34d08e6fd82eba15a0b18e37df19b6
eric <yellowtry123@gmail.com>
2020-06-05 21:07:13 +0800

    issue_list_ui

--- app/javascript/style/index.js
+++ app/javascript/style/index.js
@@ -13,7 +13,7 @@ import "./pull_request.scss";
 import "./pull_request_show.scss"
 import "./commits.scss";
 import "./commit_diff.scss";
-import "./commit-list.scss"
+import "./commit_list.scss"
 import "./public.scss";
 import "./project_setting.scss";
 import "./repo_new.scss";
