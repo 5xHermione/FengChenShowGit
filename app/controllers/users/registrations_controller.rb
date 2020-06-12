368975443265cfb2d0e3cb10c931b7b142732db9
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-06-12 12:31:26 +0800

    fix: reset all repo's remote url when user name is changed

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update
     old_user_name = current_user.name
     super
-    `mv #{ENV["GIT_SERVER_PATH"]}/#{old_user_name} #{ENV["GIT_SERVER_PATH"]}/#{params[:user][:name]}`
+    logger.info `mv #{ENV["GIT_SERVER_PATH"]}/#{old_user_name} #{ENV["GIT_SERVER_PATH"]}/#{params[:user][:name]}`
+    current_user.repositories.to_a.each do |repo|
+      logger.info "git -C #{ENV["GIT_SERVER_PATH"]}/#{params[:user][:name]}/#{repo.title} remote set-url origin #{ENV["GIT_PUSH_INSTRUCT"]}/#{params[:user][:name]}/#{repo.title}.git"
+      logger.info `git -C #{ENV["GIT_SERVER_PATH"]}/#{params[:user][:name]}/#{repo.title} remote set-url origin #{ENV["GIT_PUSH_INSTRUCT"]}/#{params[:user][:name]}/#{repo.title}.git`
+    end
   end
 
 end
